f9026753555bd15b9631738dd9d15d7e3a374267
Roman Timoshkov <timoshkow@mail.ru>
2022-06-13 18:45:55 +0300

    correct tests

--- src/test/java/ru/netology/PostmanEchoTestV1.java
+++ src/test/java/ru/netology/PostmanEchoTestV1.java
@@ -20,7 +20,7 @@ public class PostmanEchoTestV1 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
